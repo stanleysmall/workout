7bbe910195e248c7ee86383e2d1fbc493b8fa94a
Stanley Small <10047427+stanleysmall@users.noreply.github.com>
2022-12-23 10:59:47 -0500

    Remove press

--- personal.js
+++ personal.js
@@ -4,22 +4,16 @@ var one_rep_max = {
     'Squat': 220,
     'Bench': 190,
     'Deadlift': 300,
-    'Press': 95
 };
 
 var goals = {
     'Squat': 1.5,
     'Bench': 1.5,
-    'Deadlift': 2,
-    'Press': 0.8
+    'Deadlift': 2
 }
 
 var exercise_by_day = [
-    'Press',
     'Bench',
-    'Squat',
     'Deadlift',
-    'Bench',
-    'Squat',
-    'Deadlift'
+    'Squat'
 ]
